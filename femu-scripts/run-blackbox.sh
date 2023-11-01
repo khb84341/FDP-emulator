669c45ea680b1cc62c80ce9126dd5a80d8299e97
Hyunbin Kang <khb84341@gmail.com>
2023-11-01 14:05:05 +0900

    Initial Commit

---
+++ femu-scripts/run-blackbox.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Huaicheng Li <huaicheng@cs.uchicago.edu>
+# Run FEMU as a black-box SSD (FTL managed by the device)
+
+# image directory
+IMGDIR=$HOME/images
+# Virtual machine disk image
+OSIMGF=$IMGDIR/u20s.qcow2
+
+# Configurable SSD Controller layout parameters (must be power of 2)
+secsz=512 # sector size in bytes
+secs_per_pg=8 # number of sectors in a flash page
+pgs_per_blk=256 # number of pages per flash block
+blks_per_pl=256 # number of blocks per plane
+pls_per_lun=1 # keep it at one, no multiplanes support
+luns_per_ch=8 # number of chips per channel
+nchs=8 # number of channels
+ssd_size=12288 # in megabytes, if you change the above layout parameters, make sure you manually recalculate the ssd size and modify it here, please consider a default 25% overprovisioning ratio.
+
+# Latency in nanoseconds
+pg_rd_lat=40000 # page read latency
+pg_wr_lat=200000 # page write latency
+blk_er_lat=2000000 # block erase latency
+ch_xfer_lat=0 # channel transfer time, ignored for now
+
+# GC Threshold (1-100)
+gc_thres_pcent=75
+gc_thres_pcent_high=95
+
+#-----------------------------------------------------------------------
+
+#Compose the entire FEMU BBSSD command line options
+FEMU_OPTIONS="-device femu"
+FEMU_OPTIONS=${FEMU_OPTIONS}",devsz_mb=${ssd_size}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",namespaces=1"
+FEMU_OPTIONS=${FEMU_OPTIONS}",femu_mode=1"
+FEMU_OPTIONS=${FEMU_OPTIONS}",secsz=${secsz}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",secs_per_pg=${secs_per_pg}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",pgs_per_blk=${pgs_per_blk}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",blks_per_pl=${blks_per_pl}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",pls_per_lun=${pls_per_lun}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",luns_per_ch=${luns_per_ch}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",nchs=${nchs}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",pg_rd_lat=${pg_rd_lat}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",pg_wr_lat=${pg_wr_lat}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",blk_er_lat=${blk_er_lat}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",ch_xfer_lat=${ch_xfer_lat}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",gc_thres_pcent=${gc_thres_pcent}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",gc_thres_pcent_high=${gc_thres_pcent_high}"
+
+echo ${FEMU_OPTIONS}
+
+if [[ ! -e "$OSIMGF" ]]; then
+	echo ""
+	echo "VM disk image couldn't be found ..."
+	echo "Please prepare a usable VM image and place it as $OSIMGF"
+	echo "Once VM disk image is ready, please rerun this script again"
+	echo ""
+	exit
+fi
+
+sudo x86_64-softmmu/qemu-system-x86_64 \
+    -name "FEMU-BBSSD-VM" \
+    -enable-kvm \
+    -cpu host \
+    -smp 4 \
+    -m 4G \
+    -device virtio-scsi-pci,id=scsi0 \
+    -device scsi-hd,drive=hd0 \
+    -drive file=$OSIMGF,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
+    ${FEMU_OPTIONS} \
+    -net user,hostfwd=tcp::8080-:22 \
+    -net nic,model=virtio \
+    -nographic \
+    -qmp unix:./qmp-sock,server,nowait 2>&1 | tee log
---
+++ femu-scripts/run-zns.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Huaicheng Li <huaicheng@vt.edu>
+# Run FEMU as Zoned-Namespace (ZNS) SSDs
+#
+
+# Image directory
+IMGDIR=$HOME/images
+# Virtual machine disk image
+OSIMGF=$IMGDIR/u20s.qcow2
+
+if [[ ! -e "$OSIMGF" ]]; then
+	echo ""
+	echo "VM disk image couldn't be found ..."
+	echo "Please prepare a usable VM image and place it as $OSIMGF"
+	echo "Once VM disk image is ready, please rerun this script again"
+	echo ""
+	exit
+fi
+
+SSD_SIZE_MB=4096
+NUM_CHANNELS=8
+NUM_CHIPS_PER_CHANNEL=4
+READ_LATENCY_NS=40000
+WRITE_LATENCY_NS=200000
+
+FEMU_OPTIONS="-device femu"
+FEMU_OPTIONS=${FEMU_OPTIONS}",devsz_mb=${SSD_SIZE_MB}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",namespaces=1"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_ch=${NUM_CHANNELS}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_lun=${NUM_CHIPS_PER_CHANNEL}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_read=${READ_LATENCY_NS}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_write=${WRITE_LATENCY_NS}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",femu_mode=3"
+
+sudo x86_64-softmmu/qemu-system-x86_64 \
+    -name "FEMU-ZNSSD-VM" \
+    -enable-kvm \
+    -cpu host \
+    -smp 4 \
+    -m 4G \
+    -device virtio-scsi-pci,id=scsi0 \
+    -device scsi-hd,drive=hd0 \
+    -drive file=$OSIMGF,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
+    ${FEMU_OPTIONS} \
+    -net user,hostfwd=tcp::8080-:22 \
+    -net nic,model=virtio \
+    -nographic \
+    -qmp unix:./qmp-sock,server,nowait 2>&1 | tee log
